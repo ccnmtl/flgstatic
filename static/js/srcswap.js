f521ec3754d0b3afd8dd4462749928a527d689ec
zarina <zarina@columbia.edu>
2015-06-09 21:39:40 +0000

    fixed srcswap code so that video clip switching will play nice on Firefox and Safari

--- static/js/srcswap.js
+++ static/js/srcswap.js
@@ -1,37 +1,39 @@
 (function() {
-   var v = document.getElementById('vid');
-   var sources = [];
-   for (var s=0; s < v.children.length; s++) {
-      sources.push(v.children[s].src);
+    var v = document.getElementById('vid');
+    var sources = [];
+    for (var s=0; s < v.children.length; s++) {
+        sources.push(v.children[s].src);
    }
-   var labels = ['Original', 'Commentary'];
-   var activeIdx = 0;
+    var labels = ['Commentary', 'Original'];
+    var activeIdx = 0;
 
-   var p = document.getElementById('play');
-   p.addEventListener('click', function(event) {
-      if (v.paused) {
-         v.play();
-         p.innerHTML = 'pause';
-      } else {
-         v.pause();
-         p.innerHTML = 'play';
-      }
-   }, false);
+    var p = document.getElementById('play');
+    p.addEventListener('click', function(event) {
+        if (v.paused) {
+            v.play();
+            p.innerHTML = 'pause';
+        } else {
+            v.pause();
+            p.innerHTML = 'play';
+        }
+    }, false);
 
-   var b = document.getElementById('switch');
-   b.addEventListener('click', function(event) {
+    var b = document.getElementById('switch');
+    b.addEventListener('click', function(event) {
         var inactiveIdx = (activeIdx + 1) % sources.length;
         var tc = v.currentTime;
         var paused = v.paused;
         v.src = sources[inactiveIdx];
-        v.currentTime = tc;
+        v.addEventListener('loadeddata', function(){
+            v.currentTime = tc;
+        }, false);
         v.play();
         if (paused) {
-           v.pause();
+            v.pause();
         } else {
-           v.play();
+            v.play();
         }
         activeIdx = inactiveIdx;
         b.innerHTML = labels[activeIdx] + ' (switch)';
-   }, false);
+    }, false);
 })();
