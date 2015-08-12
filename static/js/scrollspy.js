53b18303d506d0fc9ed7a0ce90d84298edbe70b1
zarina <zarina@columbia.edu>
2015-08-12 14:07:35 -0400

    alphalist.js -- changed the navigation to unordered list scrollspy.js -- script for scrollspy of the index navigation scrollshrink.js -- for the nav and titlepage shrink upon scrolling down footer.html -- removed jquery call to header header.html -- added jquery call term.html and site.css -- magical spells happen here

---
+++ static/js/scrollspy.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function() {
+    var offsetHeight = 130;
+    jQuery('body').scrollspy({
+        target: "#index-list-navbar",
+        offset:offsetHeight
+    });
+    jQuery('#nav-index-list li a').click(function(event) {
+        var firstLetter = jQuery(this).parent().is(':first-child');
+        if (firstLetter) {
+            offsetHeight = 200;
+        } else {
+            offsetHeight = 130;
+        };
+        var scrollPos = jQuery('body').find(jQuery(this).attr('href')).offset().top - (offsetHeight - 1);
+        jQuery('body,html').animate({
+            scrollTop: scrollPos
+        }, 10);
+        return false;
+    });
+});
