13368970a93d86e9c8bca4dd50dae94d6eff53de
zarina <zarina@columbia.edu>
2015-08-20 16:39:22 -0400

    simple code to make homepage background images swap in and out

---
+++ static/js/bgswap.js
@@ -0,0 +1,5 @@
+(function(){
+    jQuery('#cflg-slideshow div:gt(0)').hide();
+    setInterval(function(){jQuery('#cflg-slideshow > :first-child').fadeOut(2000).next('div').fadeIn(2000).end().appendTo('#cflg-slideshow');}, 5000);
+})();
+
